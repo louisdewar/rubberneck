9eacc61ce342e3bc5a041c85498a034a84b4c130
Zak Cutner <me@zakcutner.uk>
2015-07-27 18:46:36 +0100

    Design changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if(Meteor.isClient) {
     Template.stories.helpers({
-        stories: [{location: 'Test Location', url: 'random'},{location: 'Test Location 2', url: 'random2s'},{location: 'Test Location 2', url: 'random2s'}]
+        stories: [{location: 'Calais', url: 'img-1.jpg'},{location: 'Paris', url: 'img-2.jpg'},{location: 'Istanbul', url: 'img-3.jpg'}]
     });
 }
